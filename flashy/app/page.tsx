b961ffeeb86c4d8ea11da915f495192bcdc18015
Lasse Bærland Strand <lassebs@stud.ntnu.no>
2024-02-10 15:17:52 +0100

    add more firebase functions

--- flashy/app/page.tsx
+++ flashy/app/page.tsx
@@ -4,23 +4,18 @@ import Image from "next/image";
 import { Button, Stack, Title } from "@mantine/core";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import { firestore } from "@/lib/firestore";
-import { collection, getDocs } from "@firebase/firestore";
 import { useEffect } from "react";
-import { getAllUsers } from "@/lib/utils/firebase";
+import { getAllFlashcards, getAllUsers, getFlashcardsByEmail } from "@/lib/utils/firebase";
 
 export default function Home() {
   const { data: session } = useSession();
 
   useEffect(() => {
-    // getUsers();
-
-    async function abc() {
-      console.log(await getAllUsers());
+    async function printAllFlashcards() {
+      console.log(await getAllFlashcards());
     } 
-    abc();
-      
-  }, [])
+    printAllFlashcards();
+  }, []);
 
   return (
     <Stack align="center">
--- flashy/lib/utils/firebase.ts
+++ flashy/lib/utils/firebase.ts
@@ -2,7 +2,6 @@ import { collection, getDocs } from "@firebase/firestore";
 import { firestore } from "@/lib/firestore";
 
 
-
 export async function getAllUsers(): Promise<User[]> {
     const userCollection = collection(firestore, "users");
     const docs = await getDocs(userCollection);
@@ -18,7 +17,57 @@ export async function getAllUsers(): Promise<User[]> {
             image: docData.image,
             emailVerified: docData.emailVerified,
         });
-    })
+    });
     
     return users;
+}
+
+export async function getAllFlashcards(): Promise<Flashcard[]> {
+    const flashcardCollection = collection(firestore, "flashies");
+    const docs = await getDocs(flashcardCollection);
+
+
+    const users = await getAllUsers();
+    const flashcards: Flashcard[] = [];
+
+    docs.forEach(doc => {
+        const docData = doc.data();
+
+        const creator = users.find(user => user.email == docData.creatorEmail);
+        flashcards.push({
+            creator: creator,
+            title: docData.title,
+            views: docData.views,
+        });
+    })
+
+    return flashcards;
+}
+
+export async function getFlashcardsByEmail(email: string): Promise<Flashcard[]> {
+    const flashcardCollection = collection(firestore, "flashies");
+    const docs = await getDocs(flashcardCollection);
+
+    const users = await getAllUsers();
+    const user = users.find(user => user.email == email);
+
+    if (user == null){
+        throw new Error("There is no user registered with that email");
+    }
+
+    const flashcards: Flashcard[] = [];
+
+    docs.forEach(doc => {
+        const docData = doc.data();
+
+        if (docData.creatorEmail == email){
+            flashcards.push({
+                creator: user,
+                title: docData.title,
+                views: docData.views,
+            });
+        }
+    })
+
+    return flashcards;
 }
