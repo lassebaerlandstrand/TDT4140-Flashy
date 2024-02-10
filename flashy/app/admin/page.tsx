7877dc5f0b5bbeb29040d76c3187da3ff6d8675d
ComputasMikael <mikael.steenbuch@computas.com>
2024-02-10 19:49:54 +0100

    add: usertable to /admin - remove user - change roles

---
+++ flashy/app/admin/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { LoadingOverlay, Stack, Title } from "@mantine/core";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { getAllUsers } from "@/app/utils/firebase";
+import { UsersTable } from "@/components/admin/UserTable";
+
+export default function Home() {
+  const { data: session } = useSession();
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    async function getUserTable() {
+      const users = await getAllUsers();
+      return setUsers(users);
+    }
+    getUserTable();
+  }, []);
+
+  const getCurrentActionUser = (actionUserEmail: string | undefined) => {
+    if (!actionUserEmail) return;
+    return users.find((user) => user.email === actionUserEmail);
+  };
+
+  return (
+    <Stack align="center">
+      {session ? (
+        <Stack>
+          <Title>Welcome, {session.user?.name}</Title>
+          {users.length > 0 ? (
+            <UsersTable
+              actionUser={getCurrentActionUser(session.user?.email ?? "")}
+              users={users}
+            />
+          ) : (
+            <Stack>
+              <LoadingOverlay visible />
+            </Stack>
+          )}
+        </Stack>
+      ) : (
+        <>
+          <Title>You are not an admin!</Title>
+        </>
+      )}
+    </Stack>
+  );
+}
