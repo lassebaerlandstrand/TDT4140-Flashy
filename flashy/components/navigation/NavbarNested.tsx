934de382f71f3d2a997aa93077d36e0c9331dd34
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-02-17 16:30:53 +0100

    fix: Changed to better practice implementation

--- flashy/components/navigation/NavbarNested.tsx
+++ flashy/components/navigation/NavbarNested.tsx
@@ -1,30 +1,27 @@
 import classes from "@/components/navigation/NavbarNested.module.css";
 import { UserButton } from "@/components/user/UserButton";
 import { Code, Container, Flex, Group, Space, Stack, Text, UnstyledButton } from "@mantine/core";
-import { IconCards, IconFingerprint, IconUser } from "@tabler/icons-react";
+import { IconCards, IconUser } from "@tabler/icons-react";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 
 const data = [
-  { link: "/profile", label: "Profile", icon: IconUser },
-  { link: "/flashies", label: "My flashies", icon: IconCards },
+  { link: "/profile", label: "Profile", icon: IconUser, requiresAdmin: false },
+  { link: "/flashies", label: "My flashies", icon: IconCards, requiresAdmin: false },
+  { link: "/admin", label: "Administration", icon: IconUser, requiresAdmin: true },
 ];
 
 export function NavbarNested() {
-  // if (isAdmin) {
-  //   data.push({ link: "/admin", label: "Administration", icon: IconFingerprint });
-  // }
   const session = useSession();
-  if (session?.data?.user?.role == "admin" && !data.find((item) => item.label == "Administration")) {
-    data.push({ link: "/admin", label: "Administration", icon: IconFingerprint });
-  }
-  const links = data.map((item) => (
-    <UnstyledButton component={Link} className={classes.link} href={item.link} key={item.label}>
-      <item.icon className={classes.linkIcon} stroke={1.5} />
-      <Text>{item.label}</Text>
-    </UnstyledButton>
-  ));
+  const links = data
+    .filter((item) => session.data?.user?.role == "admin" || !item.requiresAdmin)
+    .map((item) => (
+      <UnstyledButton component={Link} className={classes.link} href={item.link} key={item.label}>
+        <item.icon className={classes.linkIcon} stroke={1.5} />
+        <Text>{item.label}</Text>
+      </UnstyledButton>
+    ));
 
   return (
     <nav className={classes.navbar}>
