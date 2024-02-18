69ac00d5a3e54415f363d731fc8d3c1bb21fd462
Lasse Bærland Strand <lassebaerlandstrand@gmail.com>
2024-02-18 16:46:48 +0100

    Fix: Now retrieves correct flashcardset with error handling

--- flashy/app/carousel/[flashcardTitle]/page.tsx
+++ flashy/app/carousel/[flashcardTitle]/page.tsx
@@ -3,8 +3,9 @@
 import { FlashcardSet } from "@/app/types/flashcard";
 import { getFlashcardSet } from "@/app/utils/firebase";
 import CarouselCard from "@/components/carousel/carousel";
-import { Code, Loader, Stack, Text, Title } from "@mantine/core";
+import { Button, Code, Loader, Stack, Text, Title } from "@mantine/core";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 type FlashcardsType = {
@@ -13,6 +14,7 @@ type FlashcardsType = {
 
 export default function Flashcards({ params }: FlashcardsType) {
   const [flashcardSet, setFlashcardSet] = useState<FlashcardSet>();
+  const [failedToFetch, setFailedToFetch] = useState(false);
   const { data: session } = useSession();
 
   useEffect(() => {
@@ -20,17 +22,30 @@ export default function Flashcards({ params }: FlashcardsType) {
 
     async function fetchFlashcardSet() {
       if (session == null) return;
-      const flashcardSet = await getFlashcardSet("dummyFlashcardSet", session.user.id);
-      setFlashcardSet(flashcardSet);
+      try {
+        const decodedTitle = decodeURIComponent(params.flashcardTitle);
+        const flashcardSet = await getFlashcardSet(decodedTitle, session.user.id);
+        setFlashcardSet(flashcardSet);
+      } catch (error) {
+        setFailedToFetch(true);
+      }
     }
     fetchFlashcardSet();
   }, [session]);
 
+  if (failedToFetch) {
+    return (
+      <Stack>
+        <Title>Fant ikke flashcard settet</Title>
+        <Button component={Link} href="/" >Gå til hjemmesiden</Button>
+      </Stack>
+    );
+
+  }
+
   if (!flashcardSet) {
     return <Loader color="blue" size={48} />;
   }
-
-  console.log(flashcardSet);
 
   return (
     <Stack align="center">
